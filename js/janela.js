6c4550f217f46a9aabd459c97ca446673f176a69
VITOR SOUZA ZELLI <vitor.szelli@senacsp.edu.br>
2018-11-09 17:15:27 -0200

    Atualizado os arquivos

---
+++ js/janela.js
@@ -0,0 +1,3 @@
+var largura = (window.innerWidth > 0) ? window.innerWidth:screen.width;
+var altura = (window.innerHeight > 0) ? window.innerHeight:screen.height;
+document.write(largura + "x" + altura);
